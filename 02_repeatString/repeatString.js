83c43df9bcac2b3ad50445ca085b71ff8e4b4078
ZZpapillon <110287501+ZZpapillon@users.noreply.github.com>
2022-09-01 21:01:31 +0200

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
-};
+function repeatString(string, num) {
+       return string;
+    }
+    repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
